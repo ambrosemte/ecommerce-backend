d62a0d63f4a076bc5af453be45c1c2581e26cfba
ambrosemte <ambrosexl134@gmail.com>
2025-11-05 10:08:52 +0100

    refactor NotificationService to use aliases for Notification model and FirebaseNotification

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -5,20 +5,20 @@ namespace App\Services;
 use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification;
-
+use Kreait\Firebase\Messaging\Notification as FirebaseNotification;
+use App\Models\Notification as NotificationModel;
 class NotificationService
 {
     public function sendNotification($userId, $deviceToken, $title, $body)
     {
 
-        \App\Models\Notification::create([
+        NotificationModel::create([
             'user_id' => $userId,
             'title' => $title,
             'body' => $body
         ]);
 
-        \App\Models\Notification::where('user_id', $userId)
+        NotificationModel::where('user_id', $userId)
             ->orderByDesc('created_at')
             ->skip(30) // skip latest 30
             ->take(PHP_INT_MAX) // all older
@@ -27,7 +27,7 @@ class NotificationService
         $factory = (new Factory)->withServiceAccount(config('services.firebase.credentials'));
         $messaging = $factory->createMessaging();
 
-        $notification = Notification::create($title, $body);
+        $notification = FirebaseNotification::create($title, $body);
 
         $message = CloudMessage::withTarget('token', $deviceToken)
             ->withNotification($notification);
